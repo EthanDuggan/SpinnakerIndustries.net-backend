82ff6eb2b73ed0f7b66bc0c447148d6380694e50
Ethan Duggan <eduggan1997@gmail.com>
2022-08-25 12:58:31 -0400

    installed xlxs and express, used xlsx to load in the necessary sheets from the 'Eng Product Log.xls' workbook and convert them into arrays of JSON objects

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('TESTING!!!\n');
+const XLSX = require('xlsx');
+
+let engProductLog_xls = XLSX.readFile('//horton/reference/Eng Product Log.xls');
+let installationInstructions = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Installation Instructions']);
+let wiringDiagrams = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Wiring Diagrams']);
+
+console.log(installationInstructions);
+
+// create server for serving the api
+const express = require('express');
+
+const app = express();
+
+//app.listen(process.env.PORT || 8080, () => console.log('app available at port 8080'));
