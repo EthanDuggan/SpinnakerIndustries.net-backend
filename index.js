ff1ffe1d30d7b43e5b6e56cd2b4e8bf5ab87f55b
Ethan Duggan <eduggan1997@gmail.com>
2022-09-13 16:59:10 -0400

    implemented functionality for checking periodically for updates to the Eng Product Log.xls file.  Renamed a bunch of functions and variables and made some other minor changes in order to make more sense and to prepare for the incorporation of other data sources down the road

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 const XLSX = require('xlsx');
+const fs = require('fs');
 
 const PORT = 8081;
-const frontEndOrigin = 'http://localhost:8080'; 
+const frontEndServerOrigin = 'http://localhost:8080';
+const EngProductLogPATH = '//horton/reference/Eng Product Log.xls';
 
-let appData = loadData();
+let EngProductLogData;
+let EngProductLogLastModifiedTime;
+loadEngProductLogData();
+
 //console.log(appData.installationInstructions); // testing purposes
 
 // create server for serving the api
 const app = express();
-app.use(cors({origin: frontEndOrigin})); // adds a Cross-origin Resource Sharing (CORS) policy middleware that allows the spinnakerindustries.net front-end to make http requests to this server
+app.use(cors({origin: frontEndServerOrigin})); // adds a Cross-origin Resource Sharing (CORS) policy middleware that allows the spinnakerindustries.net front-end to make http requests to this server
 app.use(express.json()); // parses all incoming HTTP request bodies into JSON objects (by default they are just treated as strings)
 
 // routes
-app.get('/WiringDiagrams', (req, res) => {res.status(200).send(appData.wiringDiagrams);});
-app.get('/InstallationInstructions', (req, res) => {res.status(200).send(appData.installationInstructions);});
+app.get('/WiringDiagrams', (req, res) => {res.status(200).send(EngProductLogData.wiringDiagrams);});
+app.get('/InstallationInstructions', (req, res) => {res.status(200).send(EngProductLogData.installationInstructions);});
 
 app.listen(PORT, () => console.log(`app available at port ${PORT}`));
 
-function loadData() {
-    let data = {}
+//periodically check for new data from the xls file every 10 seconds
+setInterval(reloadData, 10000);
+
+
+// FUNCTIONS
+
+function reloadData() {
+    if(getLastModifiedTime(EngProductLogPATH) > EngProductLogLastModifiedTime) loadEngProductLogData();
+}
+
+function loadEngProductLogData() {
+    console.log(`loading data from ${EngProductLogPATH}`);
+    EngProductLogLastModifiedTime = getLastModifiedTime(EngProductLogPATH);
+    EngProductLogData = {};
 
     // load the "Installation Instructins" and "Wiring Diagrams" from "//horton/reference/Eng Product Log.xls" and convert them into arrays of JSON objects, where each JSON object in the array represents a row of data
-    let engProductLog_xls = XLSX.readFile('//horton/reference/Eng Product Log.xls');
-    data.installationInstructions = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Installation Instructions'], {raw: false});
-    data.wiringDiagrams = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Wiring Diagrams'], {raw: false});
+    let engProductLog_xls = XLSX.readFile(EngProductLogPATH);
+    EngProductLogData.installationInstructions = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Installation Instructions'], {raw: false});
+    EngProductLogData.wiringDiagrams = XLSX.utils.sheet_to_json(engProductLog_xls.Sheets['Wiring Diagrams'], {raw: false});
 
-    return data;
+}
+
+function getLastModifiedTime(filename) {
+    try {
+        const stats = fs.statSync(filename);
+        return stats.mtime;
+    } catch (error) {
+        console.log(error);
+    }
 }
