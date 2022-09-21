def392cc9181f308668b76a766450b311db9eb2f
Ethan Duggan <eduggan1997@gmail.com>
2022-09-21 17:15:44 -0400

    thing i did in previous commit didn't seem to work. added lines in start.sh to test whether its working

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+mkdir /test1
 mount -t cifs -o vers=3.0,username=spin.net-backend,password=wK0QDuTj3F,uid=1005,gid=1005 \\\\192.168.1.9\\reference /horton-reference
+mkdir /test2
 npm start
+mkdir /test3
